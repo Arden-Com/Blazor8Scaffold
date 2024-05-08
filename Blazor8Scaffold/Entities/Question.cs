a5ff82d4e5b0093e8779349202cf52c3ad68477e
Derek Peacock <derek.peacock@yahoo.com>
2024-05-08 11:13:22 +0100

    Added Question

--- Blazor8Scaffold/Entities/Question.cs
+++ Blazor8Scaffold/Entities/Question.cs
@@ -3,7 +3,19 @@
     public class Question
     {
         public int Id { get; set; }
-        public string Text { get; set; }
-        public string Answer { get; set; }
+        public string Text { get; set; } = string.Empty;
+        
+        public string Answer1 { get; set; } = string.Empty;
+        public bool CorrectAnswer1 { get; set; } = false;
+        
+        public string Answer2 { get; set; } = string.Empty;
+        public bool CorrectAnswer2 { get; set; } = false;
+        
+        public string Answer3 { get; set; } = string.Empty;
+        public bool CorrectAnswer3 { get; set; } = false;
+
+        public string Answer4 { get; set; } = string.Empty;
+        public bool CorrectAnswer4 { get; set; } = false;
+
     }
 }
