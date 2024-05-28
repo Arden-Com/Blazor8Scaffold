08df048f9cf4ebfa172ea50d69964bc66b68a5b6
Derek Peacock <derek.peacock@yahoo.com>
2024-05-28 10:59:11 +0100

    Annotated Question

--- Blazor8Scaffold/Entities/Question.cs
+++ Blazor8Scaffold/Entities/Question.cs
@@ -1,21 +1,32 @@
-﻿namespace Blazor8Scaffold.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blazor8Scaffold.Entities
 {
     public class Question
     {
         public int Id { get; set; }
-        public string Text { get; set; } = string.Empty;
         
+        [StringLength(100)]
+        public string Text { get; set; } = string.Empty;
+
+        [StringLength(100)]
         public string Answer1 { get; set; } = string.Empty;
         public bool CorrectAnswer1 { get; set; } = false;
-        
+
+        [StringLength(100)]
         public string Answer2 { get; set; } = string.Empty;
         public bool CorrectAnswer2 { get; set; } = false;
-        
+
+        [StringLength(100)]
         public string Answer3 { get; set; } = string.Empty;
         public bool CorrectAnswer3 { get; set; } = false;
 
+        [StringLength(100)]
         public string Answer4 { get; set; } = string.Empty;
         public bool CorrectAnswer4 { get; set; } = false;
+
+        [StringLength(200)]
+        public string Feedback { get; set; } = string.Empty;
 
     }
 }
---
+++ Blazor8Scaffold/Migrations/20240528091721_AnnotateQuestion.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blazor8Scaffold.Migrations
+{
+    /// <inheritdoc />
+    public partial class AnnotateQuestion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Text",
+                table: "Questions",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Answer4",
+                table: "Questions",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Answer3",
+                table: "Questions",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Answer2",
+                table: "Questions",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Answer1",
+                table: "Questions",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Feedback",
+                table: "Questions",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Feedback",
+                table: "Questions");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Text",
+                table: "Questions",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Answer4",
+                table: "Questions",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Answer3",
+                table: "Questions",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Answer2",
+                table: "Questions",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Answer1",
+                table: "Questions",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+    }
+}
